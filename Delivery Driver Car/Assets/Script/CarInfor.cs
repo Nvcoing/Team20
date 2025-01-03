b61889217257edcc100bee4efa0b08702ed945f8
admin <mailam493010@gmail.com>
2025-01-03 22:54:52 +0700

    'Update_Store_Car_Ver1.0'

---
+++ Delivery Driver Car/Assets/Script/CarInfor.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class CarInfor : MonoBehaviour
+{
+    public Image carImage;
+    public TMP_Text qualityCar;
+    public TMP_Text carPrice;
+    public Button btnBuy;
+    private int price;
+
+    public static PlayerScript player;
+    void Start()
+    {
+        if (player == null)
+        {
+            player = FindObjectOfType<PlayerScript>();
+        }
+    }
+
+    // Xu ly gan thong tin cho xe
+    public void setCar(Sprite image, string quality, int price)
+    {
+        carImage.sprite = image;
+        qualityCar.text = quality;
+        this.price = price;
+        carPrice.text = this.price.ToString();
+    }
+
+    // Xu  ly mua xe
+    public void OnBtnBuyClicked()
+    {
+        if (player.SpeedCar(price))
+        {
+            btnBuy.interactable = false;
+            carPrice.text = "Purchased!";
+        }
+    }
+}
