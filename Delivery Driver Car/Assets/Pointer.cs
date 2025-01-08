228b2b77b6838ca08ddaf02ac84bc4c2d48b474b
Nvcoing <Caovu9523@gmail.com>
2025-01-08 16:47:54 +0700

    update arrow

--- Delivery Driver Car/Assets/Pointer.cs
+++ Delivery Driver Car/Assets/Pointer.cs
@@ -1,23 +1,36 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Pointer : MonoBehaviour
 {
-    [SerializeField] private Transform target; 
+    [SerializeField] private Transform target;       // Mục tiêu mà mũi tên chỉ đến
+    [SerializeField] private Transform player;       // Xe hoặc đối tượng trung tâm
+    [SerializeField] private float distanceFromPlayer = 2f; // Khoảng cách từ mũi tên đến xe
+
+    private float currentAngle = 0f; // Góc hiện tại của mũi tên quanh xe
 
     void Update()
     {
-        if (target != null)
+        if (target != null && player != null)
         {
-            
-            Vector2 direction = target.position - transform.position;
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0, 0, angle - 90); 
+            // Tính góc giữa player và target
+            Vector2 direction = target.position - player.position;
+            float angleToTarget = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+            // Cập nhật góc quay
+            currentAngle = Mathf.LerpAngle(currentAngle, angleToTarget, Time.deltaTime * 5f);
+
+            // Tính vị trí mới của mũi tên dựa trên góc và khoảng cách
+            float radianAngle = currentAngle * Mathf.Deg2Rad;
+            Vector3 offset = new Vector3(Mathf.Cos(radianAngle), Mathf.Sin(radianAngle), 0) * distanceFromPlayer;
+            transform.position = player.position + offset;
+
+            // Quay mũi tên để chỉ về target
+            transform.rotation = Quaternion.Euler(0, 0, currentAngle - 90);
         }
         else
         {
-            
             gameObject.SetActive(false);
         }
     }
@@ -28,7 +41,7 @@ public class Pointer : MonoBehaviour
 
         if (target != null)
         {
-            gameObject.SetActive(true); 
+            gameObject.SetActive(true);
         }
         else
         {
