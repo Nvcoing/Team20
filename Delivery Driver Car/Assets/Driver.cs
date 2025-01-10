a5c6b4e6ec66677af16731ed17837e95bd633333
Nvcoing <Caovu9523@gmail.com>
2025-01-10 17:49:40 +0700

    update code bi xe canh sat duoi

--- Delivery Driver Car/Assets/Driver.cs
+++ Delivery Driver Car/Assets/Driver.cs
@@ -14,6 +14,7 @@ public class Driver : MonoBehaviour
     // Start is called before the first frame update
     void Start()
 	{
+
         //transform.Rotate(0,0,45);
         spriteRenderer = GetComponent<SpriteRenderer>();
         if (spriteRenderer == null)
