ee8d42d12c2ceb7781ae439de1bafbf0d170e330
Nvcoing <Caovu9523@gmail.com>
2025-01-13 15:32:09 +0700

    update code

--- Delivery Driver Car/Assets/Driver.cs
+++ Delivery Driver Car/Assets/Driver.cs
@@ -4,50 +4,70 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-	// Variables
-	[SerializeField] float steerSpeed = 300f;
-	[SerializeField] float moveSpeed = 20f;
-	[SerializeField] float slowSpeed = 15f;
-	[SerializeField] float boostSpeed = 30f;
-    private SpriteRenderer spriteRenderer; // Thêm tham chiếu đến SpriteRenderer
+    // Variables
+    [SerializeField] float steerSpeed = 300f;
+    [SerializeField] float moveSpeed = 20f;
+    [SerializeField] float slowSpeed = 15f;
+    [SerializeField] float boostSpeed = 30f;
+    private SpriteRenderer spriteRenderer;
+
+    private float normalSpeed; // Lưu trữ tốc độ bình thường
+    private Coroutine speedResetCoroutine; // Để quản lý coroutine phục hồi tốc độ
 
     // Start is called before the first frame update
     void Start()
-	{
-
-        //transform.Rotate(0,0,45);
+    {
         spriteRenderer = GetComponent<SpriteRenderer>();
         if (spriteRenderer == null)
         {
             Debug.LogError("SpriteRenderer không tìm thấy trên object xe!");
         }
+        normalSpeed = moveSpeed; // Lưu tốc độ ban đầu
     }
 
-	// Update is called once per frame
-	void Update()
-	{
-		//transform.Rotate(0,0,45);
-		//transform.Rotate(0,0,steerSpeed);
-		//transform.Translate(0,moveSpeed,0);
-		float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
-		float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
-		transform.Rotate(0, 0, -steerAmount);
-		transform.Translate(0, moveAmount, 0);
+    // Update is called once per frame
+    void Update()
+    {
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
+    }
 
-		// Time.deltatime how long each frame took to execute
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Lowering the Speed");
+        moveSpeed = slowSpeed;
 
-	}
-	void OnCollisionEnter2D(Collision2D other)
-	{
-		Debug.Log("Lowering the Speed");
-		moveSpeed = slowSpeed;
-	}
-	void OnTriggerEnter2D(Collider2D other)
-	{
-		if (other.tag == "Speed Up")
-		{
-			Debug.Log("BOOST! Speed up incoming!");
-			moveSpeed = boostSpeed;
-		}
-	}
+        // Hủy coroutine cũ nếu đang chạy
+        if (speedResetCoroutine != null)
+        {
+            StopCoroutine(speedResetCoroutine);
+        }
+
+        // Bắt đầu coroutine mới
+        speedResetCoroutine = StartCoroutine(ResetSpeedAfterDelay());
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Speed Up")
+        {
+            Debug.Log("BOOST! Speed up incoming!");
+            moveSpeed = boostSpeed;
+
+            // Hủy coroutine phục hồi tốc độ nếu đang chạy
+            if (speedResetCoroutine != null)
+            {
+                StopCoroutine(speedResetCoroutine);
+            }
+        }
+    }
+
+    IEnumerator ResetSpeedAfterDelay()
+    {
+        yield return new WaitForSeconds(5f); // Đợi 5 giây
+        moveSpeed = normalSpeed; // Phục hồi về tốc độ bình thường
+        Debug.Log("Speed restored to normal");
+    }
 }
